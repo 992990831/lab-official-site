479896c19ee0b416c995a4da0979fbead96196ec
Andy.Hu <992990831@qq.com>
2023-09-25 13:33:53 +0800

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,18 +16,38 @@ const Header = () => {
                     />
                 </a>
                 <div id="navigation">
-                    <ul className="nav-links">
-                        <Link className="nav-item" to="/">首页</Link>
-                        <Link className="nav-item" to="/research">脑科研平台</Link>
-                        <Link className="nav-item" to="/atlas">脑图谱平台</Link>
-                        <Link className="nav-item" to="/control">脑调控平台</Link>
+                    <div className="nav-links">
+                        <div>
+                        <p>&nbsp;</p> 
+                        <Link className="nav-item" to="/"> 首页  </Link>
+                        </div>
+                        <div>
+                            <p>「N3行星」</p>    
+                            <Link className="nav-item" to="/research">
+                                脑科研平台
+                            </Link>
+                        </div>
+                        <div>
+                            <p>「IC1101星系」</p>  
+                            <Link className="nav-item" to="/atlas">
+                            脑图谱平台                            
+                            </Link>
+                        </div>
+                        <div>
+                            <p>「B1919脉冲星」</p>  
+                            <Link className="nav-item" to="/control">脑调控平台</Link>
+                        </div>
+                        <div>
+                        <p>&nbsp;</p> 
                         <Link className="nav-item" to="/about">我们</Link>
+                        </div>
+                        
                         {/* <li className="nav-item"><a href="/">首页</a></li>
                         <li className="nav-item"><a href="/research">脑科研平台</a></li>
                         <li className="nav-item"><a href="/atlas">脑图谱平台</a></li>
                         <li className="nav-item"><a href="/control">脑调控平台</a></li>
                         <li className="nav-item"><a href="/about">我们</a></li> */}
-                    </ul>
+                    </div>
                 </div>
 
                 <Menu right>
